e8773f72368cef588c70931a68509aa5d23606be
kanutan93 <kanutan93@gmail.com>
2023-04-29 12:35:23 +0300

    fix SocialNetworkApplicationTests.java

--- src/test/java/ru/hl/socialnetwork/SocialNetworkApplicationTests.java
+++ src/test/java/ru/hl/socialnetwork/SocialNetworkApplicationTests.java
@@ -2,8 +2,10 @@ package ru.hl.socialnetwork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"test"})
 class SocialNetworkApplicationTests {
 
   @Test
