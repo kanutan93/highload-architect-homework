f685702df3d3b81d03af0b90b0b2977a2e2faa9e
d.morozov <d.morozov@dev.vtb.ru>
2024-02-11 20:12:59 +0200

    feat: add tarantool

--- dialog-service/src/main/java/ru/hl/dialogservice/config/DataSourceConfig.java
+++ dialog-service/src/main/java/ru/hl/dialogservice/config/DataSourceConfig.java
@@ -1,12 +1,8 @@
 package ru.hl.dialogservice.config;
 
-import io.tarantool.driver.api.TarantoolClient;
-import io.tarantool.driver.api.TarantoolClientConfig;
-import io.tarantool.driver.api.TarantoolClusterAddressProvider;
 import io.tarantool.driver.api.TarantoolServerAddress;
 import io.tarantool.driver.auth.SimpleTarantoolCredentials;
 import io.tarantool.driver.auth.TarantoolCredentials;
-import io.tarantool.driver.core.ProxyTarantoolTupleClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.tarantool.config.AbstractTarantoolDataConfiguration;
@@ -36,11 +32,5 @@ class DataSourceConfig extends AbstractTarantoolDataConfiguration {
   @Override
   public TarantoolCredentials tarantoolCredentials() {
     return new SimpleTarantoolCredentials(username, password);
-  }
-
-  @Override
-  public TarantoolClient tarantoolClient(TarantoolClientConfig tarantoolClientConfig,
-                                         TarantoolClusterAddressProvider tarantoolClusterAddressProvider) {
-    return new ProxyTarantoolTupleClient(super.tarantoolClient(tarantoolClientConfig, tarantoolClusterAddressProvider));
   }
 }
