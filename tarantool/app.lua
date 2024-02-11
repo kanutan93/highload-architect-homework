377333ef1aab3101fafdd0a4759abf8d36174127
d.morozov <d.morozov@dev.vtb.ru>
2024-02-11 20:57:50 +0200

    feat: add tarantool

--- tarantool/app.lua
+++ tarantool/app.lua
@@ -1,6 +1,6 @@
 box.cfg{}
 
-local dialog_space = box.schema.space.create('dialog', {if_not_exists = true})
+dialog_space = box.schema.space.create('dialog', {if_not_exists = true})
 dialog_space:format({
     {name = 'id', type = 'unsigned'},
     {name = 'user1_id', type = 'unsigned'},
@@ -8,9 +8,9 @@ dialog_space:format({
 })
 dialog_space:create_index('dialog_idx', {type = 'tree', parts = {'id'}}, {if_not_exists = true})
 dialog_space:create_index('dialog_comp_idx', {type = 'tree', parts = {'user1_id', 'user2_id'}}, {if_not_exists = true})
-local dialog_id_seq = box.schema.sequence.create('dialog_id_seq')
+dialog_id_seq = box.schema.sequence.create('dialog_id_sequence')
 
-local message_space = box.schema.space.create('message', {if_not_exists = true})
+message_space = box.schema.space.create('message', {if_not_exists = true})
 message_space:format({
     {name = 'id', type = 'unsigned'},
     {name = 'from_id', type = 'unsigned'},
@@ -20,23 +20,23 @@ message_space:format({
 })
 message_space:create_index('message_idx', {type = 'tree', parts = {'id', 'dialog_id'}}, {if_not_exists = true})
 message_space:create_index('message_comp_idx', {type = 'tree', parts = {'dialog_id'}}, {if_not_exists = true})
-local message_id_seq = box.schema.sequence.create('message_id_seq', {if_not_exists = true})
+message_id_seq = box.schema.sequence.create('message_id_sequence', {if_not_exists = true})
 
-local os_date = require('os').date
-local os_time = require('os').time
+os_date = require('os').date
+os_time = require('os').time
 
 function get_dialog_id(currentUserId, userId)
-    dialog_space.index.dialog_comp_idx.select {currentUserId, userId}
+    return dialog_space.index.dialog_comp_idx:select {currentUserId, userId}
 end
 
 function create_dialog(currentUserId, userId)
-    dialog_space.insert(dialog_id_seq:next(), currentUserId, userId)
+    dialog_space:insert{dialog_id_seq:next(), currentUserId, userId}
 end
 
 function get_messages(dialog_id)
-    message_space.index.message_comp_idx.select {dialogId}
+    return message_space.index.message_comp_idx:select {dialogId}
 end
 
 function create_message(fromId, toId, text, dialogId)
-    message_space.insert(message_id_seq:next(), fromId, toId, text, dialogId)
+    message_space:insert{message_id_seq:next(), fromId, toId, text, dialogId}
 end
